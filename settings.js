4b26e09f56d21ef9e26632becf0e6eaddae67695
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <fernandoagustinservinsanches@gmail.com>
2025-12-08 21:42:13 -0600

    Crear settings.js

---
+++ settings.js
@@ -0,0 +1,77 @@
+import { watchFile, unwatchFile } from "fs"
+import chalk from "chalk"
+import { fileURLToPath } from "url"
+import fs from "fs"
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumber = "" //Ejemplo: 573218138672
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+"5214181450063",
+"573153057295"
+]
+global.rowner = ["5214181450063"]
+global.suittag = ["5214181450063"] 
+global.prems = ["573114758599", "5214181450063"]
+global.fernando = ["5214181450063"]
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = "Baileys Multi Device"
+global.vs = "^2.0"
+global.nameqr = "SASUKE BOT"
+global.sessions = "Sessions/Principal"
+global.jadi = "Sessions/SubBot"
+global.AstaJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.botname = "SASUKE BOT"
+global.textbot = "SASUKE BOT • Powered By Uchiha Clan"
+global.dev = "Powered By Uchiha Clan"
+global.author = "SASUKE BOT • Sharingan Ready"
+global.etiqueta = "Sasuke"
+global.currency = "¥enes"
+
+// ⚡ LOGO CENTRALIZADO - Siempre usa lib/menu.jpg ⚡
+global.logo = fs.readFileSync('./lib/menu.jpg')
+global.banner = "https://files.catbox.moe/lajq7h.jpg"
+global.icono = "https://files.catbox.moe/lajq7h.jpg"
+global.catalogo = global.logo
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.group = "https://chat.whatsapp.com/BfCKeP10yZZ9ancsGy1Eh9"
+global.community = "https://chat.whatsapp.com/KKwDZn5vDAE6MhZFAcVQeO"
+global.channel = "https://whatsapp.com/channel/0029Vb64nWqLo4hb8cuxe23n"
+global.github = "https://github.com/Fer280809/Sasuke_bot"
+global.gmail = "fer2809fl@gmail.com"
+global.ch = {
+ch1: "120363399175402285@newsletter"
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.APIs = {
+xyro: { url: "https://xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null },
+vreden: { url: "https://api.vreden.web.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+zenzxz: { url: "https://api.zenzxz.my.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null },
+adonix: { url: "https://api-adonix.ultraplus.click", key: 'Yuki-WaBot' }
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.redBright("Update 'settings.js'"))
+import(`${file}?update=${Date.now()}`)
+})
